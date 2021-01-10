3738eb9a8d99e436de177d03aa525d451e3db3ac
LAPTOP-0T95G2JS\Yayee <rattanaprn.c@ku.th>
2021-01-10 14:20:53 +0700

    add create,get_type,single_or_double

---
+++ variable/create.py
@@ -0,0 +1,15 @@
+name = "Rattanaporn Chaithip"
+age = 21
+weight = 53
+# boolen can ishas
+can_walk = True
+can_run = False
+# is_dog
+# is_cat
+# has_pen
+
+print(name)
+print(age)
+print(weight)
+print(can_walk)
+print(can_run)
---
+++ variable/get_type.py
@@ -0,0 +1,12 @@
+name = "Rattanaporn Chaithip"
+age = 21
+weight = 53
+# boolen can ishas
+can_walk = True
+can_run = False
+
+print(type(name))
+print(type(age))
+print(type(weight))
+print(type(can_walk))
+print(type(can_run))
