f87aeca523e6f316d6167581d3d52fc6e868dd3e
LAPTOP-0T95G2JS\Yayee <rattanaprn.c@ku.th>
2021-01-17 15:33:45 +0700

    add Access Set items,Add Set items,Join Set ,Remove Set items

---
+++ Sets/Access Set items.py
@@ -0,0 +1,9 @@
+# Ex1
+thisset = {"apple", "banana", "cherry"}
+for x in thisset:
+    print(x)
+
+# EX-2
+fruits = {"apple", "banana", "cherry"}
+if "apple" in fruits:
+    print("Yes, apple is fruit!")
